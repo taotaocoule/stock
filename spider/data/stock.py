c39aabd8c62afeef3b41b8f74ff40d2f4d59f070
zhangtian3 <zhangtian3@lenovo.com>
2018-02-11 14:55:23 +0800

    add business info and theme info for stock

--- spider/data/stock.py
+++ spider/data/stock.py
@@ -13,6 +13,7 @@ class Stock(object):
 	def __init__(self, code):
 		self.code = code
 		self.market = self.code.startswith('6') and '1' or '2'
+		self.market_ = self.market == '1' and 'sh' or 'sz'
 	
 	def get(self):
 		data = list(map(lambda x:x.split(','),self.download()['data']))
@@ -25,3 +26,47 @@ class Stock(object):
 		url = 'http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?rtntype=5&token=4f1862fc3b5e77c150a2b985b12db0fd&id={}{}&type=k&authorityType=&_=1517967994707'.format(self.code,self.market)
 		raw = json.loads(urllib.request.urlopen(url).read()[1:-1])
 		return raw
+
+	def business(self):
+		url = 'http://emweb.securities.eastmoney.com/PC_HSF10/BusinessAnalysis/BusinessAnalysisAjax?code={}{}'.format(self.market_,self.code)
+		raw = json.loads(urllib.request.urlopen(url).read())
+		head = {
+			"zygc":'主营构成',
+			"zysr":'主营收入',
+			"srbl":'收入比例',
+			"zycb":'主营成本',
+			"cbbl":'成本比例',
+			"zylr":'主营利润',
+			"lrbl":'利润比例',
+			"mll":'毛利率',
+			"rq":'日期',
+			"dw":'单位'
+		}
+		# 行业分类
+		hy = []
+		# 区域分类
+		qy = []
+		# 产品分类
+		cp = []
+		for i in raw['zygcfx']:
+			hy.extend(i['hy'])
+			qy.extend(i['qy'])
+			cp.extend(i['cp'])
+		hy_pd = pd.DataFrame(hy)
+		qy_pd = pd.DataFrame(qy)
+		cp_pd = pd.DataFrame(cp)
+		result = {
+			'业务描述':raw['zyfw'][0]['ms'],
+			'经营评述':raw['jyps'][0]['ms'],
+			'行业业绩':hy_pd.rename(columns=head),
+			'区域业绩':qy_pd.rename(columns=head),
+			'产品业绩':cp_pd.rename(columns=head)
+		}
+		return result
+
+# 题材信息：http://emweb.securities.eastmoney.com/PC_HSF10/CoreConception/CoreConceptionAjax?code=sh600000
+	def theme(self):
+		url = 'http://emweb.securities.eastmoney.com/PC_HSF10/CoreConception/CoreConceptionAjax?code={}{}'.format(self.market_,self.code)
+		raw = json.loads(urllib.request.urlopen(url).read())
+		result = raw["hxtc"][0]["ydnr"].split(' ')
+		return result
