8213f4ccc05b8f26d911f3bc208b7b1641ea7f10
zhangtian3 <zhangtian3@lenovo.com>
2018-03-05 17:13:58 +0800

    all in picture and detail in excel

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ for chinese stock learning
 ##Live news
 url:http://newsapi.eastmoney.com/kuaixun/v1/getlist_102_ajaxResult_50_1_.html?r=0.29795602867346616
 
-##文件结构
+##项目结构
 daily：日报功能
 data：数据清洗
 learn：策略学习
 report：报表分析
 spider：爬虫
 strategy：完成的策略
+database：本地化的数据，csv格式
+utils：估值模型，判断模型
--- daily/daily.py
+++ daily/daily.py
@@ -1,1 +1,36 @@
 # 日报
+# 板块情况
+# 资金情况
+# 指数情况
+
+import sys
+sys.path.append("../")
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+import spider.data.market as market
+import spider.data.board as board
+
+board_data=board.Board().board().apply(pd.to_numeric,errors='ignore').sort_values('涨跌幅')
+
+# 板块总体涨跌情况
+def board_all():
+	plt.subplot(3,1,1)
+	sns.barplot(board_data['板块名称'],board_data['涨跌幅'])
+	plt.title('涨跌幅')
+	plt.xlabel("")
+	plt.ylabel("")
+	plt.xticks([])
+	plt.subplot(3,1,2)
+	sns.barplot(board_data['板块名称'],board_data['换手率'])
+	plt.xticks([])
+	plt.xlabel("")
+	plt.ylabel("")
+	plt.title('换手率')
+	plt.subplot(3,1,3)
+	sns.barplot(board_data['板块名称'],board_data['总市值'])
+	plt.xticks([])
+	plt.ylabel("")
+	plt.xlabel("")
+	plt.title('总市值')
+	plt.savefig("板块总体.jpg")
